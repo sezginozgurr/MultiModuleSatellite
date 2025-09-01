e47fe2dc2617abc4b38773e3f095371793d3b4e2
Özgür Can <sezginzgurr@gmail.com>
2025-09-02 02:43:34 +0300

    feat fix: isMinify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
             isMinifyEnabled = false
         }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
